ad7a236c5938296c1a0207ca32bd2af0efb0e1c4
foltys <folejtar.jakub@gmail.com>
2021-10-14 16:44:08 +0200

    maybe

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import PG from 'pg'
 
 const client = new PG.Client({
 	connectionString:
-		'postgres://hcklvgsanxtzdt:48e55e4258ceebe092136db6031dff3c6852256cad8e64e9dbb5be7e61b1a9c8@ec2-52-205-45-219.compute-1.amazonaws.com:5432/ddk026cb3as7re',
+		process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false,
 	},
@@ -18,14 +18,15 @@ const client = new PG.Client({
 	console.log('database connected')
 
 	app.get('/', async (req, res) => {
-		try {
-			const dbResponse = await client.query(
-				'SELECT * FROM public."Test" ORDER BY id DESC LIMIT 1',
-			)
-			res.send(dbResponse.rows)
-		} catch (e) {
-			res.send(e)
-		}
+		// try {
+		// 	const dbResponse = await client.query(
+		// 		'SELECT * FROM public."Test" ORDER BY id DESC LIMIT 1',
+		// 	)
+		// 	res.send(dbResponse.rows)
+		// } catch (e) {
+		// 	res.send(e)
+		// }
+        res.send("Hello!")
 	})
 
 	app.get('/get-all', async (req, res) => {
