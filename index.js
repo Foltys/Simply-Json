c59adedf40f86af38350eaa26f93c6dd9056a851
foltys <folejtar.jakub@gmail.com>
2021-10-14 16:27:44 +0200

    port really

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 const app = express()
 app.use(bodyParser.json())
-const port = 3000
+const port = process.env.PORT | 3000
 import PG from 'pg'
 
 const client = new PG.Client({
